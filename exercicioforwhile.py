4ddde23939f14cf0845479d9af4c31580df328a4
Trentin1 <thiagotrentini@gmail.com>
2026-03-14 13:41:11 -0300

    Exercicios de fixação de For e While

---
+++ exercicioforwhile.py
@@ -0,0 +1,10 @@
+# Exercicio 1
+# Faça um programa que mostre na tela os números de 1 até 10, um por linha
+
+# Exercicio 2
+# Crie um programa que comece com o número 1 e vá aumentande de 1 em 1 até chegar em 5,
+# mostrando cada número na tela.
+
+# Exercicio 3
+# Escreva dois programas: 1. Um com for que soma os números de 1 até 5.
+# 2. Outro com while que faz a mesma soma.
