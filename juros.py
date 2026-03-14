2f38db31f2d809d8b3178a3aba4306083c828384
Trentin1 <thiagotrentini@gmail.com>
2026-03-14 00:00:52 -0300

    Adicionando mais clareza ao código e ajeitando a taxa

--- juros.py
+++ juros.py
@@ -1,13 +1,24 @@
 # Calcule o juros de um depóisto bancário
 
 principal = float(input("Digite o valor do depósito: R$"))
-taxa = 0.01
+taxa = float(input("Digite a taxa de juros ao mês (%): "))
 tempo = float(input("Quantos meses deseja aplicar: "))
 
+# Converter a taxa percentual para decimal
+
+taxa = taxa / 100
+
 juros = principal * taxa * tempo
-print(f"Os juros acumulados serão de: R${juros:.2f}")
+
+# Uma saída com mais clareza e organizada
+
+print(f"\nDepósito inicial: R${principal:.2f}")
+print(f"Taxa de juros: {taxa*100:.2f} ao mês")
+print(f"Tempo de aplicação: {tempo: .0f} meses")
+print(f"Juros acumulados: R${juros:.2f}")
 
 # Desafio Extra!
 # Adapte o programa para que ele também calcule e exiba o valor total acumulado ao final da aplicação.
+
 valor_total = principal + juros
 print(f"O valor total ao final da aplicação será de R$ {valor_total:.2f}")
