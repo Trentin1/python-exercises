502056dfe294424ba6e70dbbc69bd8d0f3f775c8
Trentin1 <thiagotrentini@gmail.com>
2026-03-14 13:11:48 -0300

    Desafio de Laço

---
+++ desafiolaco.py
@@ -0,0 +1,8 @@
+# Escreva um programa que imprima "Hello Coddy: " e o valor de i de 3 a 27 (ambos os números inclusivos), usando um laço for.
+# Isso significa que você imprimirá 25 linhas no total, começando de 3 e terminando em 27.
+
+# A saída ficará assim:
+# Hello Coddy: 3
+# Hello Coddy: 4
+# ...
+# Hello Coddy: 27
