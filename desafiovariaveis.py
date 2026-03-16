37ad320caf9cb4cd9c29797ebbbc9f01bbb67498
Trentin1 <thiagotrentini@gmail.com>
2026-03-15 22:37:23 -0300

    Desafio de maestria variaveis

---
+++ desafiovariaveis.py
@@ -0,0 +1,9 @@
+# Escreva um script que inicializa 2 variáveis x e y com os valores 15 e 10 (respectivamente).
+
+# Após isso, inicialize outra variável z que conterá se x é menor ou igual a y.
+
+x = 15
+y = 10
+
+z = x <= y
+print(f"x = {x}, y = {y}, z = {z}")
