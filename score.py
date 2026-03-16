25953bbdb6da80cd3d6d1d7d450bb84b4a42ac2e
Trentin1 <thiagotrentini@gmail.com>
2026-03-15 22:30:50 -0300

    Revisão sobre aritimética

---
+++ score.py
@@ -0,0 +1,19 @@
+# Você recebeu um código com a inicialização de score. (Não apague esta linha!)
+
+# Sua tarefa é modificar a variável score usando atalhos aritméticos, nesta ordem:
+
+# Divida score por 2
+# Adicione 10 ao score
+# Multiplique score por 3
+
+# Lembre-se de usar os atalhos aritméticos para cada operação!
+
+
+score = 100
+
+score = score / 2
+score = score + 10
+score = score * 3
+
+
+print(f"score = {score}")
