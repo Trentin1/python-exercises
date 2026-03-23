4ed1fc29b6eca3b2b9242a9ff0472bc8f1d34dc6
Trentin1 <thiagotrentini@gmail.com>
2026-03-22 23:39:13 -0300

    Declarando uma função

---
+++ Python/Exercises/function.py
@@ -0,0 +1,13 @@
+print("Welcome to Coddy")
+print("New session...")
+print("Welcome to Coddy")
+print("Another session...")
+print("Welcome to Coddy")
+
+
+
+def greet():
+    print("Welcome to Coddy")
+    greet()
+    print("New session...")
+    greet()
